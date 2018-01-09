9c6c3c903ad7d8d9db81198b0dcccdd689d204e9
DocLJN <ljnoble@gmail.com>
2018-01-09 21:13:45 +0000

    Add book to library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> books;
+    private int capacity;
+
+    public Library(int capacity){
+        this.books = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+
+    public int countBooks() {
+        return this.books.size();
+    }
+
+    public void addBook(Book book) {
+        if (this.hasShelfSpace()) {
+            this.books.add(book);
+        }
+    }
+
+    public boolean hasShelfSpace() {
+        return this.capacity - this.countBooks() > 0;
+    }
+
+    public Book issueBook(Book book) {
+        this.books.remove(book);
+        return book;
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,53 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before(){
+        library = new Library(2);
+        book = new Book();
+    }
+
+    @Test
+    public void libraryStartsEmpty(){
+        assertEquals(0, library.countBooks());
+    }
+
+    @Test
+    public void libraryCanAddBook(){
+        library.addBook(book);
+        assertEquals(1, library.countBooks());
+    }
+
+    @Test
+    public void libraryHasShelfSpace(){
+        assertEquals(true, library.hasShelfSpace());
+    }
+
+    @Test
+    public void libraryCannotAddBookIfNoShelfSpace(){
+        for (int i = 0; i < 4; i++ ){
+            library.addBook(book);
+        }
+        assertEquals(2, library.countBooks());
+
+    }
+
+    @Test
+    public void libraryCanIssueBook(){
+        library.addBook(book);
+        library.addBook(book);
+        library.issueBook(book);
+
+        assertEquals(1, library.countBooks());
+    }
+
 }

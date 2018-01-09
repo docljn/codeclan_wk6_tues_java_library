58b695726831b88fcbe7471732ea3bc2c448ab40
DocLJN <ljnoble@gmail.com>
2018-01-09 21:47:00 +0000

    Borrower can checkout book if borrowing limit not reached.

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class Borrower {
+
+    private String name;
+    private int borrowingLimit;
+    private ArrayList<Book> checkedout;
+
+    public Borrower(String name, int borrowingLimit){
+        this.name = name;
+        this.borrowingLimit = borrowingLimit;
+        this.checkedout = new ArrayList<>();
+
+    }
+
+    public int countCheckedout() {
+        return this.checkedout.size();
+    }
+
+    public boolean canBorrowMore() {
+        return this.borrowingLimit - this.countCheckedout() > 0;
+    }
+
+    public void checkout(Book book) {
+        if (this.canBorrowMore()) {
+            this.checkedout.add(book);
+        }
+    }
 }
--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,3 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
+
+    private Borrower borrower;
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before(){
+        borrower = new Borrower("Tony", 5);
+    }
+
+    @Test
+    public void borrowerCanBorrowBooks(){
+        assertEquals(true, borrower.canBorrowMore());
+    }
+
+    @Test
+    public void borrowerCanCheckoutBook(){
+        borrower.checkout(book);
+        assertEquals(1, borrower.countCheckedout());
+    }
+
+    @Test
+    public void borrowerHasBorrowingLimit(){
+        for (int i = 0; i < 6; i++){
+            borrower.checkout(book);
+        }
+        assertEquals(5, borrower.countCheckedout());
+    }
 
 }
